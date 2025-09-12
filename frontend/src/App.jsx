8190741f0a97abc2d6c7911b04094cad327170c1
Rohit Singh <singh385536@gmail.com>
2025-09-12 16:21:46 +0530

    real url added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,8 @@ import ViewLecture from './pages/ViewLecture'
 import SearchWithAi from './pages/SearchWithAi'
 import getAllReviews from './customHooks/getAllReviews'
 
-export const serverUrl = "http://localhost:8000"
+// export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://ro-academic-x.vercel.app"
 
 function App() {
   
