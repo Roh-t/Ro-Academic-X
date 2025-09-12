d0064189b58de37cfaf62dbe334dfa44bc039e1e
Rohit Singh <singh385536@gmail.com>
2025-09-12 16:30:09 +0530

    add frontend url

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,8 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:"https://ro-academic-x-d8uc.vercel.app",
     credentials:true
 }))
 app.use("/api/auth", authRouter)
